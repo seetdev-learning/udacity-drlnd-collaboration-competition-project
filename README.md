a71dc0aa17b48efd11a4fc80e7905b5951be2d38
Darren Seet <darren@seet.dev>
2019-11-18 08:43:19 -0500

    feat: Implement ddpg for Tennis problem

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # udacity-drlnd-collaboration-competition-project
 This repository contains the code that is submitted as project 3 of Udacity's Deep Reinforcement Learning Nanodegree
+
+## Project Overview
+In this environment, two agents control rackets to bounce a ball over a net. If an agent hits the ball over the net, it receives a reward of +0.1. If an agent lets a ball hit the ground or hits the ball out of bounds, it receives a reward of -0.01. Thus, the goal of each agent is to keep the ball in play.
+
+The observation space consists of 8 variables corresponding to the position and velocity of the ball and racket. Each agent receives its own, local observation. Two continuous actions are available, corresponding to movement toward (or away from) the net, and jumping.
+
+The task is episodic, and in order to solve the environment, your agents must get an average score of +0.5 (over 100 consecutive episodes, after taking the maximum over both agents). Specifically,
+
+After each episode, we add up the rewards that each agent received (without discounting), to get a score for each agent. This yields 2 (potentially different) scores. We then take the maximum of these 2 scores.
+This yields a single score for each episode.
+The environment is considered solved, when the average (over 100 episodes) of those scores is at least +0.5.
+
+### Folder Structure
+
+## Introduction
+
+## Pre-requisites
+
+This project runs as a Jupyter notebook that is ran in a mini-conda enviroment. The following describes the instructions to run the notebook.
+
+### Installing Unity Hub
+Follow the instructions and install the Unity Hub that fits your operating system from https://store.unity.com/download
+
+### Installing Miniconda or Anaconda
+
+Anaconda is a free and open-source distribution of Python and R programming languages for scientific computing, that aims to simplify package management and deployment. Miniconda is a slim-down version of Anaconda with less packages included within the installer. The 2 should work similarly with just a different installer download. And Miniconda might require more manual installation of packages. The instructions for installation is found in https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html#regular-installation and it supports Windows, MacOS and Linux.
+
+### Constructing the Conda environment to run the project
+
+This project specific packages for execution and the versions are locked down to prevent version compatibility issues. A conda environment is constructed to host it so that the host machine is not impacted.
+
+#### Create the Conda environment
+
+Run `conda create --name drlnd-p2 python=3.6` (This is only ran once on every host)
+
+#### Activate the Conda environment
+
+Run `conda activate --name drlnd-p2` (This is ran everytime before activating the Jupyter Notebook server)
+
+#### Install the require Python packages
+
+1. Run `conda install -c conda-forge -c pytorch Pillow matplotlib numpy jupyter pytest docopt pyyaml pytorch pandas scipy ipykernel` to install the following packages:
+
+    - tensorflow
+    - protobuf
+    - grpcio
+    - Pillow
+    - matplotlib 
+    - numpy
+    - jupyter 
+    - pytest
+    - docopt 
+    - pyyaml 
+    - pytorch
+    - pandas 
+    - scipy 
+    - ipykernel
+2. Run `pip install unityagents` to install Unity Agents
+
+## Environment File Download
+
+If you wish to download the unity environment files please follow the instructions below:
+
+1. Download the environment from one of the links below.  You need only select the environment that matches your operating system:
+
+   - Linux: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Linux.zip)
+   - Mac OSX: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis.app.zip)
+   - Windows (32-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Windows_x86.zip)
+   - Windows (64-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P3/Tennis/Tennis_Windows_x86_64.zip)
+    
+    (_For Windows users_) Check out [this link](https://support.microsoft.com/en-us/help/827218/how-to-determine-whether-a-computer-is-running-a-32-bit-version-or-64) if you need help with determining if your computer is running a 32-bit version or 64-bit version of the Windows operating system.
+
+    (_For AWS_) If you'd like to train the agent on AWS (and have not [enabled a virtual screen](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Training-on-Amazon-Web-Service.md)), then please use [this link](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/one_agent/Reacher_Linux_NoVis.zip) (version 1) or [this link](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/Reacher_Linux_NoVis.zip) (version 2) to obtain the "headless" version of the environment.  You will **not** be able to watch the agent without enabling a virtual screen, but you will be able to train the agent.  (_To watch the agent, you should follow the instructions to [enable a virtual screen](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Training-on-Amazon-Web-Service.md), and then download the environment for the **Linux** operating system above._)
+
+## Running the agent
+
+1. Setup and activate the conda environment setup 
+2. Open Report.ipynb and set the `workspace` and `env_file_name` according to the runtime
+3. Run all cells in the notebook
---
+++ ddpg/agent.py
@@ -0,0 +1,199 @@
+import numpy as np
+import random
+import copy
+
+from ddpg.model import Actor, Critic
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Agent():
+    """Interacts with and learns from the environment."""
+    
+    def __init__(
+        self, device, memory, config
+    ):
+        """Initialize an Agent object.
+        
+        Params
+        ======
+            device (object): hardware device to run on CPU or GPU
+            memory (object): memory for replay buffer
+            config (dict)
+                - "state_size": dimension of each state
+                - "action_size": dimension of each action
+                - "buffer_size": replay buffer size
+                - "batch_size": minibatch size
+                - "random_seed": random seed
+                - "gamma": discount factor
+                - "tau": for soft update of target parameters
+                - "lr_actor": learning rate of the actor 
+                - "lr_critic": learning rate of the critic
+                - "weight_decay": L2 weight decay
+                - "learn_every": learn from replay buffer every time step
+                - "learn_batch_size": number of batches to learn from replay buffer every learn_every time step
+                - "grad_clip": gradient value to clip at for critic
+                - "eps_start": starting value of epsilon, for epsilon-greedy action selection
+                - "eps_end": minimum value of epsilon
+                - "eps_decay": multiplicative factor (per episode) for decreasing epsilon
+                - "print_every": Print average every x episode,
+                - "episode_steps": Maximum number of steps to run for each episode
+                - "mu": mu for noise
+                - "theta": theta for noise 
+                - "sigma": sigma for noise
+        """
+        self.num_agents = config['num_agents']
+        self.state_size = config['state_size']
+        self.action_size = config['action_size']
+        if config['random_seed'] is not  None:
+            self.seed = random.seed(config['random_seed'])
+        else:
+            self.seed = random.seed()          
+        self.eps = config['eps_start']
+        self.eps_decay = config['eps_decay']
+        self.eps_end = config['eps_end']
+
+        self.device = device
+        # Replay memory
+        self.memory = memory
+        self.batch_size = config['batch_size']
+        self.gamma = config['gamma']
+        self.tau = config['tau']
+        self.lr_actor = config['lr_actor']
+        self.lr_critic = config['lr_critic']
+        self.weight_decay = config['weight_decay']
+        self.learn_every = config['learn_every']
+        self.learn_batch_size = config['learn_batch_size']
+        self.grad_clip = config['grad_clip']
+        
+        # Actor Network (w/ Target Network)
+        self.actor_local = Actor(self.state_size, self.action_size, config['random_seed']).to(self.device)
+        self.actor_target = Actor(self.state_size, self.action_size, config['random_seed']).to(self.device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=self.lr_actor)
+
+        # Critic Network (w/ Target Network)
+        self.critic_local = Critic(self.state_size, self.action_size, config['random_seed']).to(self.device)
+        self.critic_target = Critic(self.state_size, self.action_size, config['random_seed']).to(self.device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr= self.lr_critic, weight_decay=self.weight_decay)
+
+        # Noise process
+        self.noise = OUNoise(config)
+    
+    def step(self, states, actions, rewards, next_states, dones, timestep):
+        """Save experience in replay memory, and use random sample from buffer to learn."""
+        # Save experience / reward
+        for i in range(self.num_agents):
+            self.memory.add(states[i], actions[i], rewards[i], next_states[i], dones[i])
+
+        # Learn, if enough samples are available in memory and every update_every time steps
+        if len(self.memory) > self.batch_size and timestep % self.learn_every == 0:
+            for i in range(self.learn_batch_size):
+                experiences = self.memory.sample()
+                self.learn(experiences)
+
+    def act(self, state, add_noise=True):
+        """Returns actions for given state as per current policy."""
+        state = torch.from_numpy(state).float().to(self.device)
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local(state).cpu().data.numpy()
+        self.actor_local.train()
+        if add_noise:
+            action += self.eps * self.noise.sample()
+        return np.clip(action, -1, 1)
+
+    def reset(self):
+        self.noise.reset()
+
+    def reset_episode(self):
+        self.reset()
+        self.memory.reset_episode()
+    
+    def learn_best_episode(self):
+        self.learn(self.memory.sample(True))
+    
+    def learn(self, experiences):
+        """Update policy and value parameters using given batch of experience tuples.
+        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions and Q values from target models
+        actions_next = self.actor_target(next_states)
+        Q_targets_next = self.critic_target(next_states, actions_next)
+        # Compute Q targets for current states (y_i)
+        Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
+        # Compute critic loss
+        Q_expected = self.critic_local(states, actions)
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        # gradient clipping for critic
+        if self.grad_clip > 0:
+            torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), self.grad_clip)
+        self.critic_optimizer.step()
+
+        # ---------------------------- update actor ---------------------------- #
+        # Compute actor loss
+        actions_pred = self.actor_local(states)
+        actor_loss = -self.critic_local(states, actions_pred).mean()
+        # Minimize the loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic_local, self.critic_target)
+        self.soft_update(self.actor_local, self.actor_target) 
+        
+        if self.eps_decay > 0:
+            self.eps = max(self.eps_end, self.eps - self.eps_decay)             # decrease epsilon
+            self.reset()
+        
+    def soft_update(self, local_model, target_model):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(self.tau*local_param.data + (1.0-self.tau)*target_param.data)
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, config):
+        """Initialize parameters and noise process."""
+        self.mu = config['mu'] * np.ones(config['action_size'])
+        self.theta =  config['theta']
+        self.sigma = config['sigma']
+        if config['random_seed'] is not None:
+            self.seed = random.seed(config['random_seed'])
+        else:
+            self.seed = random.seed()         
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
